c715f8ab42ce9e80ce144cb7c436af37bb975531
OttoVintola <otto.e.vintola@gmail.com>
2026-02-23 08:55:26 +0200

    submission ready

--- src/compiler/ir.py
+++ src/compiler/ir.py
@@ -38,6 +38,11 @@ class Label(Instruction):
     name: str
     
 @dataclass(frozen=True)
+class Label(Instruction):
+    """Marks the destination of a jump instruction."""
+    name: str
+    
+@dataclass(frozen=True)
 class LoadBoolConst(Instruction):
     """Loads a boolean constant value to `dest`."""
     value: bool
